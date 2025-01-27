ff2ce02506312e73160a33dfeb68bf0191721b8e
Candlewind <193207554+Candlewinds@users.noreply.github.com>
2025-01-26 23:26:04 -0800

    feat: UI implemented
    - Added TOP-standard UI buttons
    - Added round console

--- javascript.js
+++ javascript.js
@@ -8,40 +8,57 @@ function getComputerChoice() {
     : "Scissors";
 }
 
-function getHumanChoice () {
-    return prompt("What do you play?");
-}
+const playConsole = document.getElementById("playConsole")
+const scoresElement = document.getElementById("scores")
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
 function playRound (humanChoice, computerChoice) {
-    humanChoice = getHumanChoice().toLowerCase();
     computerChoice = getComputerChoice().toLowerCase();
-    console.log(humanChoice);
-    console.log(computerChoice);
+
     if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
-        console.log(`Scores are now: ${humanScore} Computer Score: ${computerScore}`);
+        playConsole.textContent = `You played ${humanChoice}. The computer played ${computerChoice}.`
+        scoresElement.textContent = `A tie! Your Score: ${humanScore} Computer Score: ${computerScore}`;
+
     } else if (
         humanChoice === "rock" && computerChoice === "scissors" || 
         humanChoice === "paper" && computerChoice === "rock" ||
         humanChoice === "scissors" && computerChoice === "paper"
         ) {
-        humanScore++
-        console.log("You win!");
-        console.log(`Scores are now: Human Score - ${humanScore} Computer Score - ${computerScore}`);
+        humanScore++;
+        playConsole.textContent = `You played ${humanChoice}. The computer played ${computerChoice}.`
+        scoresElement.textContent = `You won the round! Your Score: ${humanScore} Computer Score: ${computerScore}`;
+
         }
         else {
             computerScore++;
-            console.log("You lose!");
-            console.log(`Scores are now: ${humanScore} Computer Score: ${computerScore}`);
         }
+        playConsole.textContent = `You played ${humanChoice}. The computer played ${computerChoice}.`
+        scoresElement.textContent = `You lost the round! Your Score: ${humanScore} Computer Score: ${computerScore}`;
+
+    if (humanScore === 5) {
+        alert("You win the match!");
+        humanScore = 0;
+        computerScore = 0;
+        scoresElement.textContent = `Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    } else if (computerScore === 5) {
+        alert("You lose the match!");
+        humanScore = 0;
+        computerScore = 0;
+        scoresElement.textContent = `Your Score: ${humanScore} Computer Score: ${computerScore}`;
+    }
 }
 
-function playGame(humanChoice, computerChoice) {
-    playRound(humanChoice, computerChoice); 
-    playRound(humanChoice, computerChoice); 
-    playRound(humanChoice, computerChoice); 
-    playRound(humanChoice, computerChoice); 
-    playRound(humanChoice, computerChoice); 
-}
 
-playGame();
+rock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice);
+})
+
+paper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice);
+})
+
+scissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice);
+})
